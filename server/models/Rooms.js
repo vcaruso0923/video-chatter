e298a2c2b6ee7658834b3e0a37a5a2e3cd8f9737
vcaruso <vinnycar0923@gmail.com>
2020-10-21 14:41:31 -0700

    Models

---
+++ server/models/Rooms.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const roomsSchema = new Schema(
+    {
+        roomid: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a')
+        },
+        Name: {
+            type: String,
+            required: true
+        },
+    },
+);
+
+const Rooms = model('Rooms', roomsSchema);
+
+module.exports = Rooms;
