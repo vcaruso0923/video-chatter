b412768ff411f8892f883a3e7cbafb3bc6d3740d
vcaruso <vinnycar0923@gmail.com>
2020-10-21 14:49:29 -0700

    some typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,31 @@ const { gql } = require('apollo-server-express');
 
 // create our typeDefs
 const typeDefs = gql`
+    type Rooms {
+        _id: ID
+        roomid: String
+        createdAt: String
+        userEmail: String
+    }
+
+    type User {
+        _id: ID
+        email: String
+        rooms: [Rooms]
+        friends: [User]
+    }
+
+    type Query {
+
+    }
+
+    type Mutation {
+
+    }  
+
+    type Auth {
+        
+    }
 `;
 
 // export the typeDefs
