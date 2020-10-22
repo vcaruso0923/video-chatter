da1a72e886d0278d81ac65c086047282088985cd
osbornroxas <osbornroxas02@gmail.com>
2020-10-21 19:19:23 -0700

    create route called Dashboard, created new folder for Homepage  and reformated index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,52 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-
-// import the images for homepage
-const laptops = require('./img/laptops-wave.png');
-const devices = require('./img/devices.png');
-const computers = require('./img/computers.png');
+import Homepage from './Homepage';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
     <div className="App">
-    <Navbar sticky="top" bg="dark" variant="dark">
-      <Navbar.Brand href="#home">
-        Vroom
-      </Navbar.Brand>
+      <Router>
+        <Navbar sticky="top" bg="dark" variant="dark">
+          <Link to="/">
+            <Navbar.Brand href="#home">
+              Vroom
+            </Navbar.Brand>
+          </Link>
+
+          <Link to="/dashboard">
+            <Navbar.Text>
+              Dashboard
+            </Navbar.Text>
+          </Link>
+
       <Navbar.Collapse className="justify-content-end">
         <Navbar.Text>
           <span className="hide">Signed in as:</span> <a href="#login">Login</a>
         </Navbar.Text>
       </Navbar.Collapse>
-    </Navbar>
-    <Container fluid className="greenBg">
-      <Row className="center home-first">
-        <Col>
-          <h1>Free Video Conferencing</h1>
-          <img alt='laptops' className="img-fluid" src={String(laptops)} />
-          <br />
-          <Button variant="primary">Start A Meeting</Button>{' '}
-          <Button variant="primary">Join A Meeting</Button>{' '}
+        </Navbar>
 
-        </Col>
-      </Row>
-    </Container>
-    <Container fluid className="greenBg2">
-    <Container>
-      <Row>
-        <Col>
-        <img alt='devices' className="img-fluid" src={String(devices)} />
-        </Col>
-        <Col>
-        <h2>Device Agnostic</h2>
-        <p>Lorem to ipsum the dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud .</p>
-        </Col>
-      </Row>
-      </Container>
-    </Container>
-    <Container fluid className="greenBg">
-    <Container>
-      <Row>
-        <Col>
-        <h2>Device Agnostic</h2>
-        <p>Lorem to ipsum the dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud .</p>
-        </Col>
-        <Col>
-        <img alt='computers' className="img-fluid" src={String(computers)} />
-        </Col>
-      </Row>
-      </Container>
-    </Container>
-    
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </Router >
+
+    </div >
   );
 }
 
