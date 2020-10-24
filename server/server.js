2cb7ada1a3a95e9dbd0547bb47c1cebfefec3be5
vcaruso <vinnycar0923@gmail.com>
2020-10-24 08:38:28 -0700

    updated server file

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -7,14 +6,11 @@ const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const http = require('http').Server(app)
-const socket = require("socket.io");
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware
 });
-const io = socket(http);
 
 server.applyMiddleware({ app });
 
@@ -30,46 +26,6 @@ if (process.env.NODE_ENV === 'production') {
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-//socket logic
-const users = {};
-const socketToRoom = {};
-
-io.on('connection', socket => {
-    socket.on("join room", roomID => {
-        if (users[roomID]) {
-            const length = users[roomID].length;
-            if (length === 4) {
-                socket.emit("room full");
-                return;
-            }
-            users[roomID].push(socket.id);
-        } else {
-            users[roomID] = [socket.id];
-        }
-        socketToRoom[socket.id] = roomID;
-        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
-        socket.emit("all users", usersInThisRoom);
-    });
-
-    socket.on("sending signal", payload => {
-        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
-    });
-
-    socket.on("returning signal", payload => {
-        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
-    });
-
-    socket.on('disconnect', () => {
-        const roomID = socketToRoom[socket.id];
-        let room = users[roomID];
-        if (room) {
-            room = room.filter(id => id !== socket.id);
-            users[roomID] = room;
-        }
-    });
-
 });
 
 db.once('open', () => {
